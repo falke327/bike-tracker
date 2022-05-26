6fc99255e7c40a7e6e106ada65b864a86ad91a70
Florian Herrmann <herrmann.florian@mailbox.org>
2022-05-26 20:17:14 +0200

    Initial Commit

---
+++ src/main/java/de/falke327/biketracker/BikeTrackerApplication.java
@@ -0,0 +1,13 @@
+package de.falke327.biketracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/falke327/biketracker/BikeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.falke327.biketracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
