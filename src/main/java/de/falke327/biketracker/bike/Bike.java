65bf7a95ac220c6ad305cbc0a992af1e267a0f43
Florian Herrmann <herrmann.florian@mailbox.org>
2022-08-24 21:00:00 +0200

    Added db control to bike

--- src/main/java/de/falke327/biketracker/bike/Bike.java
+++ src/main/java/de/falke327/biketracker/bike/Bike.java
@@ -3,9 +3,9 @@ package de.falke327.biketracker.bike;
 import de.falke327.biketracker.owner.Owner;
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
 
 @ToString
 @Getter
@@ -18,11 +18,49 @@ import javax.persistence.Table;
 public class Bike {
 
     @Id
+    @SequenceGenerator(
+            name = "bike_id_seq",
+            sequenceName = "bike_id_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "bike_id_seq"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(
+            name = "bike_owner",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "bike_bike_owner_fkey"
+            )
+    )
     private Owner owner;
+
+    @Column(
+            name = "name"
+    )
     private String name;
+
+    @Column(
+            name = "vendor"
+    )
     private String maker;
+
+    @Column(
+            name = "model"
+    )
     private String model;
+
+    @Column(
+            name = "type"
+    )
     private BikeType bikeType;
 }
--- src/main/java/de/falke327/biketracker/owner/Owner.java
+++ src/main/java/de/falke327/biketracker/owner/Owner.java
@@ -1,8 +1,12 @@
 package de.falke327.biketracker.owner;
 
+import de.falke327.biketracker.bike.Bike;
 import lombok.*;
 
 import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static javax.persistence.GenerationType.SEQUENCE;
 
@@ -44,8 +48,30 @@ public class Owner {
     )
     private String lastName;
 
+    @OneToMany(
+            mappedBy = "owner",
+            orphanRemoval = true, // remove bikes when owner is removed
+            cascade = CascadeType.ALL, // always persist or remove bikes together with owner
+            fetch = FetchType.EAGER // fetch the books in same query as owners can slow down big applications
+    )
+    private List<Bike> bikes = new ArrayList<>();
+
     public Owner(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public void addBike(Bike bike) {
+        if (!this.bikes.contains(bike)) {
+            this.bikes.add(bike);
+            bike.setOwner(this);
+        }
+    }
+
+    public void removeBike(Bike bike) {
+        if (this.bikes.contains(bike)) {
+            this.bikes.remove(bike);
+            bike.setOwner(null);
+        }
     }
 }
