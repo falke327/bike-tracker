802eba773b04c2636ba5d8c646f53cb1229c595c
Florian Herrmann <herrmann.florian@mailbox.org>
2022-09-03 16:04:13 +0200

    Added Owner creation test

--- src/test/java/de/falke327/biketracker/integration/OwnerTest.java
+++ src/test/java/de/falke327/biketracker/integration/OwnerTest.java
@@ -5,14 +5,12 @@ import de.falke327.biketracker.bike.BikeType;
 import de.falke327.biketracker.owner.Owner;
 import de.falke327.biketracker.owner.OwnerRepository;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -22,9 +20,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.datasource.init.ScriptException;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -108,26 +103,38 @@ public class OwnerTest {
         NEW_TEST_OWNER.addBike(NEW_TEST_BIKE);
 
         HttpPost request = new HttpPost("http://localhost:8080/api/v1/owners/create");
-        List<NameValuePair> insertOwner = new ArrayList<>();
-        List<NameValuePair> insertBikeList = new ArrayList<>();
+        String jsonBody = createJsonBody(NEW_TEST_OWNER, NEW_TEST_BIKE);
 
-//        insertBikeList.add((new BasicNameValuePair("name", NEW_TEST_BIKE.getName())));
-//        insertBikeList.add((new BasicNameValuePair("maker", NEW_TEST_BIKE.getMaker())));
-//        insertBikeList.add((new BasicNameValuePair("model", NEW_TEST_BIKE.getModel())));
-//        insertBikeList.add((new BasicNameValuePair("bikeType", NEW_TEST_BIKE.getBikeType().toString())));
-
-        insertOwner.add(new BasicNameValuePair("firstName", NEW_TEST_OWNER.getFirstName()));
-        insertOwner.add(new BasicNameValuePair("lastName", NEW_TEST_OWNER.getLastName()));
-//        insertOwner.add(new BasicNameValuePair("bikes", insertBikeList.toString()));
-
-        request.setEntity(new UrlEncodedFormEntity(insertOwner));
+        request.setEntity(new StringEntity(jsonBody, ContentType.APPLICATION_JSON));
+        logger.info(request.getEntity().toString());
 
         // When
         final HttpResponse HTTP_RESPONSE = HttpClientBuilder.create().build().execute(request);
 
         // Then
-        assertEquals(200, HTTP_RESPONSE.getStatusLine().getStatusCode());
-        logger.info(HTTP_RESPONSE.toString());
+        assertEquals(201, HTTP_RESPONSE.getStatusLine().getStatusCode());
+        logger.info(HTTP_RESPONSE.getEntity().toString());
+
+        Owner responseOwner = RetrieveUtil.retrieveResourceFromResponse(HTTP_RESPONSE, Owner.class);
+        Owner checkOwner = ownerRepository.findById(responseOwner.getId()).get();
+
+        assertEquals(NEW_TEST_OWNER.getFirstName(), checkOwner.getFirstName());
+        assertEquals(NEW_TEST_OWNER.getLastName(), checkOwner.getLastName());
+    }
+
+    private String createJsonBody(Owner newTestOwner, Bike newTestBike) {
+        String jsonBody = "{"
+                + "\"firstName\":\"" + newTestOwner.getFirstName() + "\","
+                + "\"lastName\":\"" + newTestOwner.getLastName() + "\","
+                + "\"bikes\": [" + "{"
+                + "\"name\":\"" + newTestBike.getName() + "\","
+                + "\"maker\":\"" + newTestBike.getMaker() + "\","
+                + "\"model\":\"" + newTestBike.getModel() + "\","
+                + "\"bikeType\":\"" + newTestBike.getBikeType() + "\""
+                + "}" + "]"
+                + "}";
+        logger.info(jsonBody);
+        return jsonBody;
     }
 
     protected void initDb() throws ScriptException {
